88ddf0afce39c99cb6d10c720619a1263a28bede
delark <seroft@gmail.com>
2020-05-31 11:13:01 -0500

    avances en el curso virtual

--- cypress/integration/testWithPageObjects.js
+++ cypress/integration/testWithPageObjects.js
@@ -2,6 +2,7 @@ import { navigateTo } from '../support/page_objects/navigationPage'
 import { onFormLayoutsPage } from '../support/page_objects/formLayoutsPage'
 import { onDatepickerPage } from '../support/page_objects/datepickerPage'
 import { onFechapickerPage } from '../support/page_objects/fechapickerPage'
+import { onSmartTablePage } from '../support/page_objects/smartTablePage'
 
 describe('Test with Page Objects', () => {
 	//
@@ -25,7 +26,11 @@ describe('Test with Page Objects', () => {
 		navigateTo.datepickerPage()
 		onDatepickerPage.selectCommonDatepickerDateFromToday(14)
 		onDatepickerPage.selectDatepickerWithRangeFromToday(7, 14)
+    //onFechapickerPage.selectCommonFechapickerDateFromToday(13)
 
-		//onFechapickerPage.selectCommonFechapickerDateFromToday(13)
+    navigateTo.smartTablePage()
+    onSmartTablePage.addNewRecordWithFirstAndLastName('Héctor', 'Mera')
+    onSmartTablePage.updateAgeByFirstName('Héctor', '35')
+    onSmartTablePage.deleteRowByIndex(1)
 	})
 })
---
+++ cypress/support/page_objects/smartTablePage.js
@@ -0,0 +1,40 @@
+export class SmartTable {
+	updateAgeByFirstName(name, age) {
+		cy.get('tbody')
+			.contains('tr', name)
+			.then(tableRow => {
+				cy.wrap(tableRow).find('.nb-edit').click()
+				cy.wrap(tableRow).find('[placeholder="Age"]').clear().type(age)
+				cy.wrap(tableRow).find('.nb-checkmark').click()
+				cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+			})
+	}
+
+	addNewRecordWithFirstAndLastName(firstName, lastName) {
+		cy.get('thead').find('.nb-plus').click()
+		cy.get('thead')
+			.find('tr')
+			.eq(2)
+			.then(tableRow => {
+				cy.wrap(tableRow).find('[placeholder="First Name"]').type(firstName)
+				cy.wrap(tableRow).find('[placeholder="Last Name"]').type(lastName)
+				cy.wrap(tableRow).find('.nb-checkmark').click()
+			})
+
+		cy.get('tbody tr').first().find('td').then(tableColumns => {
+				cy.wrap(tableColumns).eq(2).should('contain', firstName)
+				cy.wrap(tableColumns).eq(3).should('contain', lastName)
+			})
+  }
+
+  deleteRowByIndex(index){
+    //2 CON ESTE METODO SE PUEDE CONFIRMAR QUE SI NO SALE LA VENTANA (window:confirm) LA PRUEBA NO PASARA YA QUE NO PODRA RETORNAR EL MENSAJE QUE DEBE MOSTRAR LA VENTANA
+		const stub = cy.stub()
+		cy.on('window:confirm', stub)
+		cy.get('tbody tr').eq(index).find('.nb-trash').click().then(() => {
+				expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+			})
+  }
+}
+
+export const onSmartTablePage = new SmartTable()
